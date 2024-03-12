80775cb52072cd790e47b4984b3d8dee63575874
mahamudhasan124 <mahamudhasan124@gmail.com>
2024-03-12 22:25:56 +0600

    Project Upload

---
+++ project/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .views import Project
+# Register your models here.
+admin.site.register(Project)
---
+++ todolist/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .views import ToDoList
+
+admin.site.register(ToDoList)
